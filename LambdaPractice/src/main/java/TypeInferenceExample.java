f1e93678b0b0b2f9ee3843d737b5027feb501e94
manan <manan@manan-PC>
2017-05-25 23:02:37 +1000

    Type Inference

--- LambdaPractice/src/main/java/TypeInferenceExample.java
+++ LambdaPractice/src/main/java/TypeInferenceExample.java
@@ -3,9 +3,15 @@ package main.java;
 public class TypeInferenceExample {
 
 	public static void main(String[] args) {
-		StringLengthLambda mylambda = (String s) -> s.length();
-		System.out.println("Lengh is: " + mylambda.getLength("Check out"));
+//		StringLengthLambda mylambda = (String s) -> s.length();
+//		System.out.println("Lengh is: " + mylambda.getLength("Check out"));
+		
+		printLambda(s -> s.length());
 
+	}
+	
+	public static void printLambda(StringLengthLambda l){
+		System.out.println(l.getLength("whatever"));
 	}
 	
 	interface StringLengthLambda {
